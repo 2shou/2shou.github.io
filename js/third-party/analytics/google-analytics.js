930153b122a9364cd99449ac18b10c512270236c
2shou <gavin.zgz@gmail.com>
2023-05-23 23:19:32 +0800

    Site updated: 2023-05-23 23:19:31

--- js/third-party/analytics/google-analytics.js
+++ js/third-party/analytics/google-analytics.js
@@ -1,35 +1,9 @@
-/* global CONFIG, dataLayer, gtag */
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-8EM0R3YL47"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-if (!CONFIG.google_analytics.only_pageview) {
-  if (CONFIG.hostname === location.hostname) {
-    window.dataLayer = window.dataLayer || [];
-    window.gtag = function() {
-      dataLayer.push(arguments);
-    };
-    gtag('js', new Date());
-    gtag('config', CONFIG.google_analytics.tracking_id);
-
-    document.addEventListener('pjax:success', () => {
-      gtag('event', 'page_view', {
-        page_location: location.href,
-        page_path    : location.pathname,
-        page_title   : document.title
-      });
-    });
-  }
-} else {
-  const sendPageView = () => {
-    if (CONFIG.hostname !== location.hostname) return;
-    const uid = localStorage.getItem('uid') || (Math.random() + '.' + Math.random());
-    localStorage.setItem('uid', uid);
-    navigator.sendBeacon('https://www.google-analytics.com/collect', new URLSearchParams({
-      v  : 1,
-      tid: CONFIG.google_analytics.tracking_id,
-      cid: uid,
-      t  : 'pageview',
-      dp : encodeURIComponent(location.pathname)
-    }));
-  };
-  document.addEventListener('pjax:complete', sendPageView);
-  sendPageView();
-}
+  gtag('config', 'G-8EM0R3YL47');
+</script>
